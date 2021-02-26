56325514455ef500982e11a9b4c95ec13d3cd9a4
guilherme.092011 <guilherme.092011@hotmail.com>
2021-02-26 16:15:07 -0300

    nlw dia 5 finalizado

--- moveit-next/src/components/ExperienceBar.tsx
+++ moveit-next/src/components/ExperienceBar.tsx
@@ -4,8 +4,9 @@ import styles from '../styles/components/ExperienceBar.module.css'
 
 export function ExperienceBar() {
   const { currentExperience, experienceToNextLevel } = useContext(ChallengesContext);
-
+  console.log(currentExperience, experienceToNextLevel)
   const percentToNextLevel = Math.round((currentExperience * 100) / experienceToNextLevel);
+  console.log(percentToNextLevel);
   return (
     <header className={styles.experienceBar}>
       <span>0 xp</span>
